b1629ab969b486426339ae824ee9a4e3e8d9b931
Scott Roberts <s.e.roberts0@gmail.com>
2022-07-22 14:09:53 -0700

    Alter global font per tailwind docs

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['pokemon-gb', 'ui-sans-serif', 'system-ui']
+      }
+    }
   },
   plugins: []
 }
